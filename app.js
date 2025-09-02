ff9a56bb908c897138d0117699361170d8314e13
ShahbazShaddy <shahbazghafil@gmail.com>
2025-09-02 14:10:06 +0500

    Enhance static asset handling with cache control in app.js and update vercel.json for improved routing

--- app.js
+++ app.js
@@ -20,9 +20,6 @@ require("./config/passport.js")(passport);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(expressLayouts);
-app.use("/assets", express.static(path.join(__dirname, "assets")));
-
-// With this:
 app.use("/assets", express.static(path.join(__dirname, "assets"), {
   maxAge: '1d',
   immutable: true
